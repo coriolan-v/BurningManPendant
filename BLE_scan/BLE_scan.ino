668d2407616c0026c5ca27c9c4ace04ef8ea75ce
Cori V <21335163+coriolan-v@users.noreply.github.com>
2023-08-02 21:58:18 +0100

    updates on the pcb and fw

---
+++ BLE_scan/BLE_scan.ino
@@ -0,0 +1,32 @@
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  setupSleep();
+
+  initBLE();
+
+
+}
+
+void loop() {
+  scanBLE();
+}
